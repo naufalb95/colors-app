a866130edf52c1619c292fcf7fa19580d36a1ed6
Naufal Budiman <982654ant@gmail.com>
2021-05-02 18:48:16 +0700

    adding color helper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Palette from './Palette';
 
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelper';
 
 import './App.css';
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
       <Palette {...seedColors[4]} />
