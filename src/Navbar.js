109d197a57a90be2eb4295a37e94d02c2b789b20
Naufal Budiman <982654ant@gmail.com>
2021-06-21 07:20:17 +0700

    Refactoring Navbar CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,62 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/styles';
 import { IconButton } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
+
+const styles = {
+  Navbar: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '6vh'
+  },
+  logo: {
+    marginRight: '15px',
+    padding: '0 13px',
+    fontSize: '22px',
+    height: '100%',
+    background: '#eceff1',
+    display: 'flex',
+    alignItems: 'center',
+    fontFamily: 'Roboto',
+    '& a': {
+      textDecoration: 'none',
+      color: 'black'
+    }
+  },
+  slider: {
+    width: '340px',
+    display: 'inline-block',
+    margin: '0 10px',
+    '& .rc-slider-rail ': {
+      height: '8px'
+    },
+
+    '& .rc-slider-track': {
+      backgroundColor: 'transparent'
+    },
+
+    '& .rc-slider-handle, .rc-slider-handle:focus, .rc-slider-handle:hover, .rc-slider-handle:active':
+      {
+        backgroundColor: 'green',
+        outline: 'none',
+        border: '2px solid green',
+        width: '13px',
+        height: '13px',
+        marginLeft: '-7px',
+        marginTop: '-3px',
+        boxShadow: 'none'
+      }
+  },
+  selectContainer: {
+    margin: '0 1rem 0 auto'
+  }
+};
 
 class Navbar extends Component {
   constructor(props) {
@@ -32,17 +83,17 @@ class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevelHandler, isSingleColor } = this.props;
+    const { level, changeLevelHandler, isSingleColor, classes } = this.props;
     const { format } = this.state;
     return (
-      <nav className="Navbar">
-        <div className="logo">
+      <nav className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to="/">reactcolorpicker</Link>
         </div>
         {isSingleColor || (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -53,7 +104,7 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select defaultValue="hex" onChange={this.changeFormatHandler}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -87,4 +138,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
