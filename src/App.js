8762fad529c59c458daa974a8cf8d9937044aea4
Naufal Budiman <982654ant@gmail.com>
2021-06-29 14:29:31 +0700

    Adding NewPaletteForm and Link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingeColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
@@ -18,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/styles';
 
@@ -17,6 +18,7 @@ class PaletteList extends Component {
           <div className={classes.container}>
             <div className={classes.nav}>
               <h1>React Colors</h1>
+              <Link to="/palette/new">Create Palette</Link>
             </div>
             <div className={classes.palettes}>
               {palettes.map((palette) => (
